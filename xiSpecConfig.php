40dd482e7ca032a9b2bd7c4fa769a82e2923fab4
root <root@xi4.bio.ed.ac.uk>
2021-03-08 14:09:48 +0000

    update file size limit message

--- xiSpecConfig.php
+++ xiSpecConfig.php
@@ -1,4 +1,4 @@
 <?php
-echo "CLMSUI.xiSpecBaseDir = \"/xiUI_public/spectrum/\";\n"; // you may need to edit this depending on where xiView_container is in web server folder
-echo "CLMSUI.xiAnnotRoot = \"http://localhost/xiAnnotator/\";\n";
+echo "CLMSUI.xiSpecBaseDir = \"/spectrum/\";\n"; // you may need to edit this depending on where xiView_container is in web server folder
+echo "CLMSUI.xiAnnotRoot = \"https://xiview.org/xiAnnotator/\";\n";
 ?>
