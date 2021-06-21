a17c7850c45b68a59510d44357dbf9b83208015a
colin-combe <colin.combe@ed.ac.uk>
2021-06-21 15:15:33 +0100

    es6 wip, tests (mainly) passing

--- webpack.common.js
+++ webpack.common.js
@@ -3,24 +3,24 @@ const path = require("path");
 
 
 module.exports = {
-    // entry: path.resolve(__dirname + "/CLMS-model/src/search-results-model.js"),
-    // output: {
-    //     path: path.resolve(__dirname, "dist"),
-    //     filename: "clms-model.js",
-    //     library: ["clmsModel"],
-    //     libraryTarget: "umd"
-    // },
-
-    entry: {
-        clms: './CLMS-model/src/search-results-model.js',
-        xinet: './crosslink-viewer/src/crosslink-viewer-BB.js',
-    },
+    entry: "./xi3/js/main.js",
     output: {
-        filename: '[name].js',
-        path: __dirname + '/dist',
-        library: ['[name]'],
+        path: path.resolve(__dirname, "dist"),
+        filename: "xiview.js",
+        library: ["xiview"],
         libraryTarget: "umd"
     },
+
+    // entry: {
+    //     clms: './CLMS-model/src/search-results-model.js',
+    //     xinet: './crosslink-viewer/src/crosslink-viewer-BB.js',
+    // },
+    // output: {
+    //     filename: '[name].js',
+    //     path: __dirname + '/dist',
+    //     library: ['[name]'],
+    //     libraryTarget: "umd"
+    // },
 
     module: {
         rules: [
